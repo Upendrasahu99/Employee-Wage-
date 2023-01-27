3db339e8272821db29b2370827be65fd021a6975
upendra <upendrasahu1199@gmail.com>
2023-01-27 13:49:43 +0530

    Added : Employee is Present or Absent

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -5,8 +5,16 @@ import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        Random random = new Random();
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        int fullDayWage = wagePerHour * fullDayHour;
+        int a = random.nextInt(2);
+        if (a == 0) {
+            System.out.println("Employee is absent");
+        } else {
+            System.out.println("Employee is present than daily wage  = " + fullDayWage);
+        }
 
     }
 }
-
-
