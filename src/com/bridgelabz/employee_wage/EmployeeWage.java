47aa667cbbb9d141632421a42b04e846e06c3851
upendra <upendrasahu1199@gmail.com>
2023-01-27 14:05:21 +0530

    Added : Using Switch Case Statment

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -10,12 +10,18 @@ public class EmployeeWage {
         int fullDayHour = 8;
         int partTimeHour = 4;
         int a = random.nextInt(3);
-        if (a == 0) {
-            System.out.println("Employee is absent");
-        } else if (a == 1) {
-            System.out.println("Employee is present full day than daily wage is = " + fullDayHour * wagePerHour);
-        } else {
-            System.out.println("Employee is present part time than daily wage is = " + partTimeHour * wagePerHour);
+        switch (a) {
+            case 0:
+                System.out.println("Employee is absent");
+                break;
+            case 1:
+                System.out.println("Employee is present for full day than daily wage  = " + wagePerHour * fullDayHour);
+                break;
+            default:
+                System.out.println();
+                System.out.println("Employee is present for part time than daily wage = " + wagePerHour* partTimeHour);
+
         }
     }
+
 }
