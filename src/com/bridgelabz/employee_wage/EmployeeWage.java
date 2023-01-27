d6329d7a74d4defb003cb3d22374a72eebc0f6e6
upendra <upendrasahu1199@gmail.com>
2023-01-27 13:46:20 +0530

    Welcome to Employee Wage Computation

---
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.employee_wage;
+
+import java.util.Random;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+    }
+}
+
+
