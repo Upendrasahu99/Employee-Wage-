20dda682cfba8c81bbbae7da023031032f8f3bf5
upendra <upendrasahu1199@gmail.com>
2023-01-27 14:07:17 +0530

    Added : Calculating Wages for a Month

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -8,20 +8,30 @@ public class EmployeeWage {
         Random random = new Random();
         int wagePerHour = 20;
         int fullDayHour = 8;
+        int fullDayWage = wagePerHour*fullDayHour;//Daily wage
         int partTimeHour = 4;
-        int a = random.nextInt(3);
-        switch (a) {
-            case 0:
-                System.out.println("Employee is absent");
-                break;
-            case 1:
-                System.out.println("Employee is present for full day than daily wage  = " + wagePerHour * fullDayHour);
-                break;
-            default:
-                System.out.println();
-                System.out.println("Employee is present for part time than daily wage = " + wagePerHour* partTimeHour);
+        int partTimeWage = wagePerHour*partTimeHour;//Daily wage
+        int absentHour =0;
+        int perMonthWorkingDay = 20;
+        int fullDayCount =0;
+        int partTimeCount = 0;
+        for (int i = 1; i <= perMonthWorkingDay; i++) {
+            System.out.print(" Day " + i );
+            int a = random.nextInt(3);
+            switch (a) {
+                case 0:
+                    System.out.println(" Employee is absent than day " + i + " Employee wage is " + absentHour);
+                    break;
+                case 1:
+                    System.out.println(" Employee is present for full day " + i + " Employee wage is " + fullDayWage );
+                    fullDayCount++;
+                    break;
+                default:
+                    System.out.println(" Employee is present for Part Time " + i + " Employee wage is " + partTimeWage);
+                    partTimeCount++;
 
+            }
         }
+        System.out.println("Per month wage = "+ ((fullDayCount*fullDayWage)+(partTimeCount*partTimeWage)));
     }
-
 }
