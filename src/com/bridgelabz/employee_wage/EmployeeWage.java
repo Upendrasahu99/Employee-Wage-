99ada37121eb76cccc378604b44d74259fbf8022
upendra <upendrasahu1199@gmail.com>
2023-01-27 13:58:02 +0530

    Added : Daily Employee Wage

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -6,13 +6,17 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         Random random = new Random();
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        int fullDayWage = wagePerHour * fullDayHour;
         int a = random.nextInt(2);
         if (a == 0) {
             System.out.println("Employee is absent");
         } else {
-            System.out.println("Emplyee is present");
+            System.out.println("Employee is present than daily wage  = " + fullDayWage);
         }
 
     }
 }
+
 
